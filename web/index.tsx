3689ee45cc4ac507a84887e2fbc0bb22018134db
wongchichong <wongchichong@gmail.com>
2023-05-22 14:34:10 +0800

    web playground 2

--- web/index.tsx
+++ web/index.tsx
@@ -139,7 +139,7 @@ export default function MyApp() {
                     <path d="M0 0v85.498h71.166V99.83H128V85.498h128V0H0z" fill="#CB3837" />
                     <path d="M42.502 14.332h-28.17v56.834h28.17V28.664h14.332v42.502h14.332V14.332H42.502zM85.498 14.332v71.166h28.664V71.166h28.17V14.332H85.498zM128 56.834h-13.838v-28.17H128v28.17zM184.834 14.332h-28.17v56.834h28.17V28.664h14.332v42.502h14.332V28.664h14.332v42.502h14.332V14.332h-57.328z" fill="#FFF" />
                 </svg></a></h1>
-        <h4>React here:</h4>
+        <h4>Paste your React here:</h4>
         <CodeEditor
             value={code}
             language="js"
